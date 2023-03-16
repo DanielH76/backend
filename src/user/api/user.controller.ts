742dd44abe5b3ed513b53c324e85f13622a78826
DanielH76 <daniel.hammershoi@gmail.com>
2023-03-16 09:28:58 +0100

    feat: find user by id

--- src/user/api/user.controller.ts
+++ src/user/api/user.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, Post } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Get, NotFoundException, Param, Post } from "@nestjs/common";
 import { User } from "../service/schemas/user.schema";
 import { UserService } from "../service/user.service";
 
@@ -12,10 +12,21 @@ export class UserController {
     async create(@Body() user: User){
         if(!user) return BadRequestException
 
-        this.userService.create(user)
-        console.log(user)
-        return user
+        const createdUser = await this.userService.create(user)
+        console.log(createdUser)
+        return createdUser
 
+    }
+
+    @Get(':id')
+    async get(@Param() params){
+        if(!params.id) return 'Missing id'
+
+        const userToFind = await this.userService.get(params.id)
+        console.log(userToFind)
+
+        if(!userToFind) return NotFoundException
+        return userToFind
     }
 
 
