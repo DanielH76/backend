1a2c263f98dabf29de41e8105d381f4b009b5622
DanielH76 <daniel.hammershoi@gmail.com>
2023-03-16 09:29:01 +0100

    find by id

--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User, UserDocument } from "./schemas/user.schema";
@@ -14,10 +14,21 @@ export class UserService {
         try{
             const user = await this.userModel.create(userToCreate);
             user.save()
-        }catch(e) {
-            return e.message 
+        }catch(error) {
+            return error.message 
         }
     }
 
-    
+    async get(id: string) {
+        if(!id) return null
+
+        try{
+            const userToFind = await this.userModel.findById(id)
+            if(!userToFind) return NotFoundException
+            return userToFind
+        }catch(error) {
+            return error.message
+        }
+        
+    }
 }
